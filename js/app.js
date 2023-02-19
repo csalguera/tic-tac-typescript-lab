075b8978b815ad6ae3c7060eaf0db933d37c049b
Carlos Salguera <carlos.e.salguera@gmail.com>
2023-02-18 19:25:20 -0500

    define winningCombos array

--- js/app.js
+++ js/app.js
@@ -1,5 +1,15 @@
 "use strict";
 // Constants ----------------------------------
+const winningCombos = [
+    0, 1, 2,
+    3, 4, 5,
+    6, 7, 8,
+    0, 3, 6,
+    1, 4, 7,
+    2, 5, 8,
+    0, 4, 8,
+    2, 4, 6,
+];
 // Variables ----------------------------------
 let board, turn, winner, tie;
 // Cached Element References ------------------
@@ -20,6 +30,8 @@ function init() {
 }
 init();
 function render() {
+    updateBoard();
+    updateMessage();
 }
 function updateBoard() {
     board.forEach((val, idx) => {
--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,16 @@
 // Constants ----------------------------------
+
+const winningCombos: number[] = [
+  0, 1, 2,
+  3, 4, 5,
+  6, 7, 8,
+  0, 3, 6,
+  1, 4, 7,
+  2, 5, 8,
+  0, 4, 8,
+  2, 4, 6,
+]
+
 // Variables ----------------------------------
 
 let board: number[],
@@ -30,7 +42,8 @@ function init():void {
 init()
 
 function render():void {
-
+  updateBoard()
+  updateMessage()
 }
 
 function updateBoard():void {
