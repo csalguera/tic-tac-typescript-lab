658042d076c37188ecbbbb7b5a5e5247f29fb63d
Carlos Salguera <carlos.e.salguera@gmail.com>
2023-02-19 12:35:04 -0500

    let combo be implicit

--- js/app.js
+++ js/app.js
@@ -1,14 +1,14 @@
 "use strict";
 // Constants ----------------------------------
 const winningCombos = [
-    0, 1, 2,
-    3, 4, 5,
-    6, 7, 8,
-    0, 3, 6,
-    1, 4, 7,
-    2, 5, 8,
-    0, 4, 8,
-    2, 4, 6,
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
 ];
 // Variables ----------------------------------
 let board, turn, winner, tie;
@@ -78,7 +78,7 @@ function checkForTie() {
     tie = true;
 }
 function checkForWinner() {
-    winningCombos.forEach((combo) => {
+    winningCombos.forEach(combo => {
         if (Math.abs(board[combo[0]] + board[combo[1]] + board[combo[2]]) === 3) {
             winner = true;
         }
--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,14 @@
 // Constants ----------------------------------
 
-const winningCombos: number[] = [
-  0, 1, 2,
-  3, 4, 5,
-  6, 7, 8,
-  0, 3, 6,
-  1, 4, 7,
-  2, 5, 8,
-  0, 4, 8,
-  2, 4, 6,
+const winningCombos: number[][] = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
 ]
 
 // Variables ----------------------------------
@@ -96,8 +96,8 @@ function checkForTie():void {
   tie = true
 }
 
-function checkForWinner():void {
-  winningCombos.forEach((combo: any) => {
+function checkForWinner(): void {
+  winningCombos.forEach(combo => {
     if (Math.abs(board[combo[0]] + board[combo[1]] + board[combo[2]]) === 3) {
       winner = true
     }
